2e7907bc5af00c41249331e8276099a80e125ff9
Grifold Studio <jb3278@columbia.edu>
2021-07-23 11:24:30 +0900

    Review ENG complete

---
+++ src/components/VIP/pages/vip_testing.js
@@ -0,0 +1,36 @@
+
+
+
+{/* 
+            <div className='video-responsive2-vip'>
+                <iframe 
+                    className='iframe'
+                    src="https://player.vimeo.com/video/494030007?title=0&byline=0&portrait=0" 
+                    width="640" 
+                    height="360" 
+                    frameBorder="0" 
+                    allow="autoplay; fullscreen; picture-in-picture" 
+                    allowFullScreen
+                    title="So"
+                    >                    
+                </iframe>
+            </div> */}
+
+            {/* <div className='video-responsive3-vip'>             
+                <FullwidthHorizontal item={Kan1961JP} scale="70" />
+                <HalfwidthHorizontal item1={Kan1965JP} item2={Datsu1963JP} scaleLeft={54} contentWidth={59} />
+                <HalfwidthHorizontal item1={Kanzan1964JP} item2={Usobuku_shikkin1963JP} scaleLeft={40} contentWidth={46} />
+
+                <div className='containerstyle notFirst' >
+                    <img src={group7} alt="" lang='jp'/>
+                </div>
+
+                <FullwidthHorizontal item={Kumomushin1967JP} scale="60" />
+                <FullwidthHorizontal item={Eko1967JP} scale="60" />
+
+                <InquiryJP />
+
+                <a href='/vip2' className='nextJp'>
+                    <h2>次のページ</h2>
+                </a>
+            </div>  */}
